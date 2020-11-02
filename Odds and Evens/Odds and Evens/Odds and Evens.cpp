0559708a5275a7f07d0b36e1903f1d6bcfacedfb
David Levi <dal0531@my.londonmet.ac.uk>
2020-11-02 16:32:49 +0000

    Odds and Evens: Complete

--- Odds and Evens/Odds and Evens/Odds and Evens.cpp
+++ Odds and Evens/Odds and Evens/Odds and Evens.cpp
@@ -6,8 +6,15 @@ const int TOTAL = 10;
 int main()
 {
     
-    int numbers[TOTAL] = { 78, 42, 7, 8, 15, 2, 83, 64, 23, 13 };
-
-    for ()
+    int numbers[TOTAL] = {78, 42, 7, 8, 15, 2, 83, 64, 23, 13};
+    for (int i = 0; i < TOTAL; i++)
+        if (numbers[i] % 2 == 0)
+        {
+            std::cout << "The number " << numbers[i] << " is even." << std::endl;
+        }
+        else
+        {
+            std::cout << "The number " << numbers[i] << " is odd." << std::endl;
+        }
 }
 
